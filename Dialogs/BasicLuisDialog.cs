bb18f8ccd71e9ffac7a3285d37202c551b9312b0
Szymon Pulka <szympulka@gmail.com>
2017-11-26 10:20:26 +0100

    mm

---
+++ Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Microsoft.Bot.Sample.LuisBot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"], ConfigurationManager.AppSettings["LuisAPIKey"])))
+        {
+        }
+
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+          await context.SayAsync("Sunny day in rawicz: "+ context.Activity.ChannelId, "Sunny day in rawicz: " + context.Activity.ChannelId);
+            context.Wait(MessageReceived);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
+        [LuisIntent("Book")]
+        public async Task Book(IDialogContext context, LuisResult result)
+        {
+            await context.SayAsync("OK! i will book hotel for you", "OK! i will book hotel for you");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Calendar")]
+        public async Task Calendar(IDialogContext context, LuisResult result)
+        {
+            await context.SayAsync("You have free day today", "You have free day today");
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("Weather.GetCondition")]
+        public async Task WeatherGetCondition(IDialogContext context, LuisResult result)
+        {
+            await context.SayAsync("Weather. GetCondition", "Weather. GetCondition");
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("Weather.GetForecast")]
+        public async Task WeatherGetForecast(IDialogContext context, LuisResult result)
+        {
+            await context.SayAsync("Weather. GetForecast", "Weather. GetForecast");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=iothubapp-Web-Deploy.pubxml -p:Password=0YFwzdw8kzGebTnJ48gTqHFfFsMfp6MMxug6fKZTrjs42x0H0tSjJgjiHyDf
+
